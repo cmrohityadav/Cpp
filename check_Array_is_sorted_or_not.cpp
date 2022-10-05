3afc7518208de41e5a4ee246e762ce4ae0444e05
Rohit  Chhabiraj Yadav <cmrohityadav23@gmail.com>
2022-10-05 23:13:07 +0530

    Add files via upload

---
+++ check_Array_is_sorted_or_not.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+bool sortfun(int arr[],int n){
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i]>arr[i-1])
+        {
+           return true;
+        }
+    }
+        return false;
+    
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+      cin>>arr[i];
+    }
+    if (sortfun(arr,n)==1)
+    {
+        cout<<"array is sorted";
+    }
+    else{
+        cout<<"array is not sorted";
+    }
+    
+
+    return 0;
+}
